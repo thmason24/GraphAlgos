8bf3952c4e54df7e7dc51b3d57fc00c8f0ca7fb6
Tim Mason <millionsofbillions@gmail.com>
2017-03-27 19:35:37 -0700

    submitted passed clustering.py
    Completed Required portions of class

--- clustering.py
+++ clustering.py
@@ -2,9 +2,65 @@
 import sys
 import math
 
+
+def distance(v1,v2,x,y):
+    return math.sqrt((x[v1]-x[v2])**2 +  (y[v1]-y[v2])**2) 
+    
+
 def clustering(x, y, k):
     #write your code here
-    return -1.
+
+    #create edge list
+    edges = []
+    for i in range(len(x)):
+        for j in range(i,len(x)):
+            if i != j:
+                edges.append([i,j,distance(i,j,x,y)])
+
+    #sort edges
+    sorted_Edges = sorted(edges, key=lambda tup: tup[2])
+    
+    for i in sorted_Edges:
+        pass #print(i)
+ 
+
+    #Krusgal's algorthm       
+    membership = range(n)
+    #for i in membership:
+    #    print(i)
+    #print()
+    
+    
+    #run Kruskal algorithm
+    MST = []  #initialize minimum spanning tree
+    minDist = 0
+    for i in sorted_Edges:
+
+        #make sure vertices are not already joined
+        if membership[i[0]] != membership[i[1]]:
+            #stop if number of partitions eqals desired number of clusters
+            if len(set(membership)) == k:
+                nextEdge = i
+                break
+            #add edge
+            MST.append(i)
+            minDist += i[2]
+            #join groups
+            membership = list(map(lambda x: membership[i[0]] if x == membership[i[1]] else x, membership))
+    #print()
+    #print(nextEdge)    
+    #print(i)
+            
+            
+            
+                    
+        
+        
+        
+        
+        
+        
+    return nextEdge[2]
 
 
 if __name__ == '__main__':
